cb70f3a957bff9a588e37d75131cd7f3532e8600
Brennan <brennanterre@gmail.com>
2023-03-28 14:17:37 +0800

    updated

--- connect.php
+++ connect.php
@@ -1,38 +1,5 @@
-<?php 
-
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $db = "contact_form";
-
-    $connection = mysqli_connect("localhost","root","");
-    $db = mysqli_select_db($connection, 'contact_form');
-
-   // if (!$connection) {
-    //    die ("Connection Failed: " . mysqli_connect_error());
-    //}
-   // echo "Connected Successfully!";
-
-
-if(isset($_POST['submit'])) {
-    $Name = $_POST['name'];
-    $Email = $_POST['email'];
-    $Message = $_POST['message'];
-
-
-    $query = "INSERT INTO feedback (`Name`, `Email`, `Message`) VALUES ('$Name', '$Email', '$Message')";
-    $query_run = mysqli_query($connection, $query);
-
-if ($query_run)
-    {
-        echo '<script> alert ("COMMENTED"); </script>';
-        header('Location: index.php');
-    }
-    else{echo '<script> alert("ERROR"); </script>';
-} 
+<?php
+$connection = mysqli_connect("localhost", "root", "", "usa_tolentino");
+if (!$connection) {
+   die(" Connection Error ");
 }
-
-?>
-
-
-
