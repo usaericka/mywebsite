67a5fce828fb649e4f6cdc76372842cf83c56b2d
efaye <eftolentino@usa.edu.ph>
2023-03-30 01:30:17 +0800

    changes and added

--- view.php
+++ view.php
@@ -1,4 +1,4 @@
-<?php include "./dbs/dbs.php"; ?>
+<?php include "./dbs/dbs.php";?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Submitted Form</title>
+    <title>Contact</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
    <link rel="stylesheet" href="./styles/style.css">
@@ -16,19 +16,19 @@
 </head>
 <body>
     <div class="container">
-        <div class="box">
-            <h4 class="display-4 text-center"></h4><hr>
-                <a href="index.php" class="back text-dark">Home</a> 
+            <h2 class="display-5 text-center mb-3">Responses</h2><hr>
                 <?php if (mysqli_num_rows($result)) { ?>
-                <table class="table table-striped">
+                <table class="table table-bordered table-dark table-hover">
                     <thead>
-                        <tr>
-                            <th scope = "col">ID</th>
+                        <tr class="text-center">
+                            <th scope = "col">#</th>
                             <th scope = "col">Name</th>
                             <th scope = "col">Email</th>
                             <th scope = "col">Message</th>
+                            <th colspan="2">Actions</th>
                         </tr>
                     </thead>
+
                     <tbody>
                     <?php
                         $i = 0;
@@ -40,16 +40,25 @@
                             <td><?=$rows['name'];?></td>
                             <td><?php echo $rows['email']; ?></td>
                             <td><?php echo $rows['message']; ?></td>
+
+                            <td class="text-center">
+                                <form action="update.php" method="post">
+                                    <input type="hidden" name="id" value="<?php echo $rows['id'] ?>">
+                                    <input type="submit" name="update" class="btn btn-primary" value="EDIT">
+                                </form>
+                            </td>
+
+                            <td class="text-center">
+                                <form action="delete.php" method="post">
+                                    <input type="hidden" name="id" value="<?php echo $rows['id']?>">
+                                    <input type="submit" name="delete" class="btn btn-danger" value="DELETE">
+                                </form>
+                            </td>
                         </tr>
                         <?php } ?>
                     </tbody>
                 </table>
-            <?php } ?>
-        </div>
-    </div>                 
+                <?php } ?>
+    </div>               
 </body>
 </html>
-
-
-
-           
